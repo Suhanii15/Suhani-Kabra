a632c9259a862f851e7b0adec7b4d76f7455c519
Suhanii15 <suhanikabra931@gmail.com>
2026-07-07 21:42:53 +0530

    basics

---
+++ suhani/src/components/navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Smile } from 'lucide-react';
+
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center  justify-between rounded-3xl sticky z-50 mx-20 my-5 bg-slate-900 px-6 py-4 text-white shadow-md">
+      <div className="text-2xl font-signature text-yellow-300 tracking-widest">Suhani</div>
+      <div className="flex gap-8">
+        <a href="#home" className="text-blue-300 hover:text-blue-400">Home</a>
+        <a href="#about" className="text-blue-300 hover:text-blue-400">Projects</a>
+        <a href="#contact" className="text-blue-300 hover:text-blue-400">Resume</a>
+        <a href="#about" className="text-blue-300 hover:text-blue-400">About Me</a>
+      </div>
+      <div className=" flex flex-row bg-yellow-400 font-bold cursor-pointer text-slate-900 rounded-full px-3 py-1 hover:bg-yellow-300">
+        Contact Me
+        <Smile className="ml-2" />
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ suhani/src/pages/landingPage.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+import Navbar from '../components/navbar.jsx'
+import profileImg from '../assets/Photo.jpeg'
+import {motion } from 'framer-motion'
+
+const landingPage = () => {
+  return (
+    <section className="min-h-screen bg-slate-950 bg-[#14141D]  p-4">
+        {/* Background Pattern */}
+        <div 
+        className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#80808025_1px,transparent_1px),linear-gradient(to_bottom,#80808025_1px,transparent_1px)] bg-[size:60px_60px]" 
+        aria-hidden="true"
+      />
+
+ <Navbar />
+ {/* Main Content */}
+      <div className="relative z-10 text-center mt-30">
+
+
+
+{/* headingss */}
+<div className="flex flex-col items-center justify-center gap-6 ">
+        <h1 className="text-4xl font-semibold text-white mb-4 tracking-wider">
+            Building <span className="bg-pink-400 text-slate-900 rounded-full px-3 py-1">Scalable Software</span> & 
+        </h1>
+        <h1 className="text-4xl font-semibold text-white tracking-wider">
+            Interactive Digital <span className="relative inline-block px-4 py-4 border border-zinc-700 bg-zinc-900 backdrop-blur-xs text-yellow-300 ">Experiences
+              <div className="absolute -top-1 -left-1 w-1.5 h-1.5 border border-zinc-500 bg-[#14141D]" />
+              <div className="absolute -top-1 -right-1 w-1.5 h-1.5 border border-zinc-500 bg-[#14141D]" />
+              <div className="absolute -bottom-1 -left-1 w-1.5 h-1.5 border border-zinc-500 bg-[#14141D]" />
+              <div className="absolute -bottom-1 -right-1 w-1.5 h-1.5 border border-zinc-500 bg-[#14141D]" />
+               <div className="absolute -bottom-5 -right-5 flex flex-col items-start select-none">
+                <svg 
+                  width="54" 
+                  height="44" 
+                  viewBox="0 0 24 24" 
+                  fill="none" 
+                  className="drop-shadow-md"
+                >
+                  <path 
+                    d="M4.5 3V19.5L9.75 14.25H18.75L4.5 3Z" 
+                    fill="#bae6fd" 
+                    stroke="#0284c7" 
+                    strokeWidth="1.5" 
+                    strokeLinejoin="round"
+                  />
+                  <path 
+                    d="M17 14V20M14 17H20" 
+                    stroke="#bae6fd" 
+                    strokeWidth="2" 
+                    strokeLinecap="round"
+                  />
+                </svg>
+              </div>
+            </span>
+            
+        </h1>
+</div>
+
+{/* About Section */}
+<div className="flex flex-row mt-15 items-center justify-center gap-12">
+    <div>
+        <img 
+        src={profileImg}
+        alt="Suhani Kabra" 
+        className="w-64 h-64  object-cover"
+        />
+    </div>
+
+    <div className="flex flex-col items-start gap-4 max-w-xl">
+<h2 className="text-white text-left">
+Hi, I am Suhani Kabra, a software developer and  tech enthusiast. I come up with strong problem solving skills in DSA and Competitive Programming.
+I have hands on experience in building real-world projects based on Full Stack, Generative AI and  Machine Learning.
+</h2>
+<button className="rounded-full mt-4 font-semibold text-lg shadow-md bg-yellow-400 text-slate-900 px-3 py-2 hover:bg-yellow-300 transition-colors duration-300 cursor-pointer" >
+    View Resume
+</button>
+
+    </div>
+
+</div>
+
+ </div>
+
+
+   
+    </section>
+  )
+}
+
+export default landingPage
