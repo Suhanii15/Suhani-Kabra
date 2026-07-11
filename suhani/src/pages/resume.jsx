a5e67e4dddd5d1484e365ab8da4cde78cf8ca331
Suhanii15 <suhanikabra931@gmail.com>
2026-07-11 10:48:48 +0530

    added resume drive link

--- suhani/src/pages/resume.jsx
+++ suhani/src/pages/resume.jsx
@@ -87,6 +87,7 @@ const personal=[
        I'm always excited to tackle new challenges and build solutions that make an impact.
     </motion.p>
     <div className="relative inline-block group">
+      <a href="https://drive.google.com/file/d/1bBqhUq30FwYVdH8t2hU-xtgSTDLJdZYj/view?usp=drive_link">
 <motion.button 
  variants={slideUp}
       whileHover={pillHover}
@@ -96,7 +97,7 @@ const personal=[
  hover:bg-yellow-300 transition-colors duration-300 cursor-pointer" >
     Download Resume
 </motion.button>
- 
+ </a>
 </div>
    </div>
 
